de1dccd50096080913cb20694dc95f26d9cb8af0
Thi004 <phamthuymanthi@gmail.com>
2024-08-05 08:11:34 +0700

    outlet 03-8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Order from "./components/Order";
+import './Home.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Routes>
+                <Route element={<Login/>} path=''></Route>
+                <Route element={<Register/>} path='register'></Route>
+                <Route element={<Home/>} path='home'>
+                    <Route element={<Product/>} path= ''></Route>
+                    <Route element={<Order/>} path= 'order'></Route>
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import {Link, Outlet, useLocation} from "react-router-dom";
+
+function Home() {
+
+    return (
+        <>
+            <div>
+                <div className={'header'}>
+                    <div className={'logo'}>Logo</div>
+                    <div className={'banner'}>Banner</div>
+                </div>
+                <div className={'menu'}>
+                    <div className={'menu-left'}>
+                        <div className="home menu-item"><Link to={'/home'}>Home</Link></div>
+                        <div className="product menu-item">Product</div>
+                    </div>
+                    <div className={'menu-right'}>
+                        <div className="order menu-item"><Link to={'/home/order'}>Order</Link></div>
+                        <div className="menu-item" ><Link to={'/'}>Logout</Link></div>
+                    </div>
+
+                </div>
+                <div className={'body'}>
+                    <div className="sidebar">Side bar</div>
+                    <div className="listProduct">
+                        <Outlet></Outlet>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home
